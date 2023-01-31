6f70a909589be8070c8bbd6b32022208c26cd55f
psykomicron <psykomicron@protonmail.com>
2023-01-31 13:05:28 +0100

    Refactors

--- Croak/HotkeyViewModel.cpp
+++ Croak/HotkeyViewModel.cpp
@@ -4,6 +4,26 @@
 #include "HotkeyViewModel.g.cpp"
 #endif
 
+using namespace winrt::Windows::System;
+
+
 namespace winrt::Croak::implementation
 {
+    VirtualKeyModifiers HotkeyViewModel::GetVirtualModifiers(const uint32_t& value)
+    {
+        return 
+            ((value & MOD_CONTROL) ? VirtualKeyModifiers::Control : VirtualKeyModifiers::None) |
+            ((value & MOD_ALT)     ? VirtualKeyModifiers::Menu    : VirtualKeyModifiers::None) |
+            ((value & MOD_SHIFT)   ? VirtualKeyModifiers::Shift   : VirtualKeyModifiers::None) |
+            ((value & MOD_WIN)     ? VirtualKeyModifiers::Windows : VirtualKeyModifiers::None);
+    }
+
+    uint32_t HotkeyViewModel::GetModifiers(const VirtualKeyModifiers& virtualModifiers)
+    {
+        return
+            ((virtualModifiers & VirtualKeyModifiers::Control) == VirtualKeyModifiers::Control ? MOD_CONTROL : 0) |
+            ((virtualModifiers & VirtualKeyModifiers::Menu)    == VirtualKeyModifiers::Menu    ? MOD_ALT     : 0) |
+            ((virtualModifiers & VirtualKeyModifiers::Shift)   == VirtualKeyModifiers::Shift   ? MOD_SHIFT   : 0) |
+            ((virtualModifiers & VirtualKeyModifiers::Windows) == VirtualKeyModifiers::Windows ? MOD_WIN     : 0);
+    }
 }
--- Croak/HotkeyViewModel.h
+++ Croak/HotkeyViewModel.h
@@ -8,55 +8,62 @@ namespace winrt::Croak::implementation
     {
     public:
         HotkeyViewModel() = default;
-        HotkeyViewModel(const winrt::hstring& hotKeyName, const bool& enabled, const winrt::Windows::System::VirtualKey& key, const winrt::Windows::System::VirtualKeyModifiers& modifiers) :
+        HotkeyViewModel(const winrt::hstring& hotKeyName, const bool& enabled, const uint32_t& key, const uint32_t& modifiers) :
             _hotKeyName(hotKeyName),
             _isHotKeyEnabled(enabled),
-            _key(key),
-            _modifiers(modifiers)
+            key(key),
+            modifiers(modifiers)
         {
-        };
+        }
 
-        inline winrt::Windows::System::VirtualKeyModifiers Modifiers() const
+        inline winrt::Windows::System::VirtualKeyModifiers Modifiers()
         {
-            return _modifiers;
-        };
+            return GetVirtualModifiers(modifiers);
+        }
+
         inline void Modifiers(const winrt::Windows::System::VirtualKeyModifiers& value)
         {
-            _modifiers = value;
-        };
+            modifiers = GetModifiers(value);
+        }
 
-        inline winrt::Windows::System::VirtualKey Key() const
+        inline winrt::Windows::System::VirtualKey Key()
         {
-            return _key;
-        };
+            return static_cast<winrt::Windows::System::VirtualKey>(key);
+        }
+
         inline void Key(const winrt::Windows::System::VirtualKey& value)
         {
-            _key = value;
-        };
+            key = static_cast<uint32_t>(value);
+        }
 
         inline bool IsHotKeyEnabled() const
         {
             return _isHotKeyEnabled;
-        };
+        }
+
         inline void IsHotKeyEnabled(const bool& value)
         {
             _isHotKeyEnabled = value;
-        };
+        }
 
         inline winrt::hstring HotKeyName() const
         {
             return _hotKeyName;
-        };
+        }
+
         inline void HotKeyName(const winrt::hstring& value)
         {
             _hotKeyName = value;
-        };
+        }
 
     private:
-        winrt::Windows::System::VirtualKeyModifiers _modifiers = winrt::Windows::System::VirtualKeyModifiers::None;
-        winrt::Windows::System::VirtualKey _key = winrt::Windows::System::VirtualKey::None;
+        uint32_t modifiers = 0u;
+        uint32_t key = 0u;
         bool _isHotKeyEnabled = false;
         winrt::hstring _hotKeyName{};
+
+        Windows::System::VirtualKeyModifiers GetVirtualModifiers(const uint32_t& value);
+        uint32_t GetModifiers(const Windows::System::VirtualKeyModifiers& modifier);
     };
 }
 

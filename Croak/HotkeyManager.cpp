f6878e9d3bd7d21918c8a8fe40246853729157fa
psykomicron <psykomicron@protonmail.com>
2023-01-31 12:47:46 +0100

    Implemented HotKeyManager & added GetManaged key to list hot keys

--- Croak/HotkeyManager.cpp
+++ Croak/HotkeyManager.cpp
@@ -9,57 +9,87 @@ using namespace winrt::Windows::System;
 
 namespace Croak::System::HotKeys
 {
-	HotKeyManager::~HotKeyManager()
-	{
-		// TODO: Take read-write lock.
-		for (auto&& pair : hotKeys)
-		{
-			delete pair.second;
-		}
-	}
+    HotKeyManager::~HotKeyManager()
+    {
+        // TODO: Take read-write lock.
+        for (auto&& pair : hotKeys)
+        {
+            delete pair.second;
+        }
+    }
 
-	HotKeyID HotKeyManager::RegisterHotKey(const VirtualKeyModifiers& modifiers, const uint32_t& virtualKey, const bool& isRepeating)
-	{
-		// TODO:
-		// - Check arguments validity
-		// - Activate and throw properly if the key has failed to activate.
+    void HotKeyManager::RegisterHotKey(const VirtualKeyModifiers& modifiers, const uint32_t& virtualKey, const bool& isRepeating, const HotKeyId& agnosticId)
+    {
+        // TODO:
+        // - Check arguments validity
+        // - Activate and throw properly if the key has failed to activate.
 
-		HotKey* hotKey = new HotKey(modifiers, virtualKey, isRepeating);
+        HotKey* hotKey = new HotKey(modifiers, virtualKey, isRepeating);
 
-		hotKey->Activate();
+        try
+        {
+            hotKey->Activate(); // throws
+        }
+        catch (const std::invalid_argument&)
+        {
+            hotKey->Enabled(false);
+            throw;
+        }
 
-		hotKey->Fired([this, id = hotKey->KeyId()](auto, auto)
-		{
-			e_hotKeyFired(id, nullptr);
-		});
+        hotKey->Fired([this, id = agnosticId](auto, auto)
+        {
+            e_hotKeyFired(id, nullptr);
+        });
 
-		hotKeys.insert({ hotKey->KeyId(), hotKey});
-		return hotKey->KeyId();
-	}
+        hotKeys.insert({ agnosticId, hotKey});
+    }
 
-	void HotKeyManager::EditKey(const HotKeyID& hotKeyId, const VirtualKeyModifiers& modifiers, const uint32_t& virtualKey)
-	{
-		if (hotKeys.find(hotKeyId) != hotKeys.cend())
-		{
-			//TODO: Don't reallocate memory, make the HotKey able to re-register itself with new parameters.
-			unique_ptr<HotKey> hotKeyPtr{ hotKeys[hotKeyId] };
-			if (hotKeyPtr.get())
-			{
-				//DebugLog(format(L"Replacing hot key (id: {0}).", hotKey->Id()));
-				HotKey* newHotKey = new HotKey(modifiers, virtualKey, false);
-				newHotKey->Activate();
+    void HotKeyManager::EditKey(const HotKeyId& hotKeyId, const VirtualKeyModifiers& modifiers, const uint32_t& virtualKey)
+    {
+        if (hotKeys.find(hotKeyId) != hotKeys.cend())
+        {
+            //TODO: Don't reallocate memory, make the HotKey able to re-register itself with new parameters.
+            HotKey* hotKey = hotKeys[hotKeyId];
+            if (hotKey)
+            {
+                DebugLog(format("Replacing hot key (id: {0}).", hotKey->KeyId()));
 
-				newHotKey->Fired([this, id = newHotKey->KeyId()](auto, auto)
-				{
-					e_hotKeyFired(id, nullptr);
-				});
+                HotKey* newHotKey = new HotKey(modifiers, virtualKey, false);
+                newHotKey->Activate();
 
-				hotKeys[hotKeyId] = newHotKey;
-			}
-		}
-	}
+                delete hotKey;
 
-	void HotKeyManager::ReplaceOrInsertKey(HotKey* previousKey, HotKey* newKey)
-	{
-	}
+                newHotKey->Fired([this, id = hotKeyId](auto, auto)
+                {
+                    e_hotKeyFired(id, nullptr);
+                });
+
+                hotKeys[hotKeyId] = newHotKey;
+            }
+        }
+    }
+
+    void HotKeyManager::EditKey(const::Croak::System::HotKeys::HotKeyId& agnosticId, const bool& state)
+    {
+        if (hotKeys.find(agnosticId) != hotKeys.cend())
+        {
+            hotKeys[agnosticId]->Enabled(state);
+        }
+    }
+
+    void HotKeyManager::ReplaceOrInsertKey(HotKey* previousKey, HotKey* newKey)
+    {
+    }
+
+    map<HotKeyId, HotKeyView> HotKeyManager::GetManagedKeys()
+    {
+        map<HotKeyId, HotKeyView> view{};
+
+        for (auto&& pair : hotKeys)
+        {
+            view.insert({ pair.first, HotKeyView(pair.second, pair.first) });
+        }
+
+        return view;
+    }
 }
--- Croak/HotkeyManager.h
+++ Croak/HotkeyManager.h
@@ -1,60 +1,66 @@
 #pragma once
-#include <ppl.h>
 #include <concurrent_unordered_map.h>
 #include "HotKey.h"
+#include "HotKeyView.h"
 
 namespace Croak::System::HotKeys
 {
-	typedef uint32_t HotKeyID;
+    typedef uint32_t HotKeyId;
 
-	/**
-	 * @brief Singleton class to manage hotkeys.
-	*/
-	class HotKeyManager
-	{
-	public:
-		/**
-		 * @brief Copy constructor.
-		*/
-		HotKeyManager(const HotKeyManager& other) = delete;
-		~HotKeyManager();
+    /**
+     * @brief Singleton class to manage hotkeys.
+    */
+    class HotKeyManager
+    {
+    public:
+        /**
+         * @brief Copy constructor.
+        */
+        HotKeyManager(const HotKeyManager& other) = delete;
+        ~HotKeyManager();
 
-		static HotKeyManager& GetHotKeyManager()
-		{
-			static HotKeyManager instance{};
-			return instance;
-		}
+        static HotKeyManager& GetHotKeyManager()
+        {
+            static HotKeyManager instance{};
+            return instance;
+        }
 
-		inline winrt::event_token HotKeyFired(const winrt::Windows::Foundation::TypedEventHandler<HotKeyID, winrt::Windows::Foundation::IInspectable>& handler)
-		{
-			return e_hotKeyFired.add(handler);
-		}
+        inline winrt::event_token HotKeyFired(const winrt::Windows::Foundation::TypedEventHandler<HotKeyId, winrt::Windows::Foundation::IInspectable>& handler)
+        {
+            return e_hotKeyFired.add(handler);
+        }
 
-		inline void HotKeyFired(const winrt::event_token& token)
-		{
-			e_hotKeyFired.remove(token);
-		}
+        inline void HotKeyFired(const winrt::event_token& token)
+        {
+            e_hotKeyFired.remove(token);
+        }
 
-		::Croak::System::HotKeys::HotKeyID RegisterHotKey(const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& virtualKey, const bool& isRepeating);
-		void EditKey(const ::Croak::System::HotKeys::HotKeyID& hotKeyId, const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& virtualKey);
-		void ReplaceOrInsertKey(HotKeys::HotKey* previousKey, HotKeys::HotKey* newKey);
+        /**
+         * @brief Registers a hot key, allowing it to be managed by the HotKeyManager.
+         * @param modifiers Windows, Control, Alt, Shift
+         * @param virtualKey Virtual key code (VK_[...])
+         * @param isRepeating If set to true, the event will be repeating as long as the user presses the key combo.
+         * @param agnosticId Identifier for the caller to recognize that hot key when fired.
+        */
+        void RegisterHotKey(const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& virtualKey, const bool& isRepeating, const HotKeyId& agnosticId);
+        void EditKey(const ::Croak::System::HotKeys::HotKeyId& agnosticId, const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& virtualKey);
+        void EditKey(const ::Croak::System::HotKeys::HotKeyId& agnosticId, const bool& state);
+        void ReplaceOrInsertKey(::Croak::System::HotKeys::HotKey* previousKey, ::Croak::System::HotKeys::HotKey* newKey);
+        std::map<HotKeyId, ::Croak::System::HotKeys::HotKeyView> GetManagedKeys();
 
-		/**
-		 * @brief Copy operator.
-		*/
-		HotKeyManager& operator=(const HotKeyManager& other) = delete;
+        /**
+         * @brief Copy operator.
+        */
+        HotKeyManager& operator=(const HotKeyManager& other) = delete;
 
-	private:
-		Concurrency::concurrent_unordered_map<HotKeyID, HotKeys::HotKey*> hotKeys{};
-		Concurrency::concurrent_unordered_map<HotKeyID, std::function<void()>> callbacks{};
-		winrt::event<winrt::Windows::Foundation::TypedEventHandler<HotKeyID, winrt::Windows::Foundation::IInspectable>> e_hotKeyFired{};
+    private:
+        Concurrency::concurrent_unordered_map<HotKeyId, HotKeys::HotKey*> hotKeys{};
+        winrt::event<winrt::Windows::Foundation::TypedEventHandler<HotKeyId, winrt::Windows::Foundation::IInspectable>> e_hotKeyFired{};
 
-		/**
-		 * @brief Default constructor.
-		*/
-		HotKeyManager() = default;
-
-		inline GUID CreateGuid();
-	};
+        /**
+         * @brief Default constructor.
+        */
+        HotKeyManager() = default;
+    };
 }
 

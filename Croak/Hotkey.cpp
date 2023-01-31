d87562a506464d0c344fa412d5e11146b769933c
psykomicron <psykomicron@protonmail.com>
2023-01-31 12:46:40 +0100

    Updated invalid key detection, other small refactors

--- Croak/Hotkey.cpp
+++ Croak/Hotkey.cpp
@@ -7,102 +7,101 @@ using namespace winrt::Windows::System;
 
 namespace Croak::System::HotKeys
 {
-	std::atomic_int32_t HotKey::globalIds = 1;
+    std::atomic_int32_t HotKey::globalIds = 1;
 
-	HotKey::HotKey(const VirtualKeyModifiers& modifiers, const uint32_t& key, const bool& isRepeating) :
-		key{ key },
-		repeating{ isRepeating }
-	{
-		hotKeyId = globalIds.fetch_add(1);
+    HotKey::HotKey(const VirtualKeyModifiers& modifiers, const uint32_t& key, const bool& isRepeating) :
+        key{ key },
+        repeating{ isRepeating }
+    {
+        hotKeyId = globalIds.fetch_add(1);
 
-		if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Control))
-		{
-			this->modifiers |= MOD_CONTROL;
-		}
-		if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Menu))
-		{
-			this->modifiers |= MOD_ALT;
+        if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Control))
+        {
+            this->modifiers |= MOD_CONTROL;
+        }
+        if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Menu))
+        {
+            this->modifiers |= MOD_ALT;
 
-		}
-		if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Shift))
-		{
-			this->modifiers |= MOD_SHIFT;
-		}
-		if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Windows))
-		{
-			this->modifiers |= MOD_WIN;
-		}
-	}
+        }
+        if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Shift))
+        {
+            this->modifiers |= MOD_SHIFT;
+        }
+        if (static_cast<uint32_t>(modifiers & VirtualKeyModifiers::Windows))
+        {
+            this->modifiers |= MOD_WIN;
+        }
+    }
 
-	HotKey::~HotKey()
-	{
-		if (notificationThread != nullptr)
-		{
-			PostThreadMessage(threadId, WM_QUIT, 0, 0); // Post quit message to the HotKey thread (GetMessage will return 0).
-			notificationThread->join(); // Wait for the thread to exit.
-			delete notificationThread; // Free the memory.
+    HotKey::~HotKey()
+    {
+        if (notificationThread != nullptr)
+        {
+            PostThreadMessage(threadId, WM_QUIT, 0, 0); // Post quit message to the HotKey thread (GetMessage will return 0).
+            notificationThread->join(); // Wait for the thread to exit.
+            delete notificationThread; // Free the memory.
 
-			DebugLog(std::format("Successfully free resources (key id: {0})", std::to_string(hotKeyId)));
-		}
-	}
+            DebugLog(std::format("Successfully free resources (key id: {0})", std::to_string(hotKeyId)));
+        }
+    }
 
-	void HotKey::Activate()
-	{
-		std::atomic_flag startFlag{};
+    void HotKey::Activate()
+    {
+        std::atomic_flag startFlag{};
+        bool failed = true;
 
-		notificationThread = new std::thread(&HotKey::ThreadFunction, this, &startFlag);
+        notificationThread = new std::thread(&HotKey::ThreadFunction, this, &startFlag, &failed);
 
-		startFlag.wait(false); // Wait to check if the HotKey was registered successfully or not.
-		if (!startFlag.test()) // If the test returns false it means the HotKey registration has failed, throw exception.
-		{
-			throw std::invalid_argument("Hot key is not valid (rejected by the system).");
-		}
-	}
+        startFlag.wait(false); // Wait to check if the HotKey was registered successfully or not.
+        if (failed) // If the test returns false it means the HotKey registration has failed, throw exception.
+        {
+            throw std::invalid_argument("Hot key is not valid (rejected by the system).");
+        }
+    }
 
 
-	void HotKey::ThreadFunction(std::atomic_flag* startFlag)
-	{
-		threadId = GetCurrentThreadId();
+    void HotKey::ThreadFunction(std::atomic_flag* startFlag, bool* failed)
+    {
+        threadId = GetCurrentThreadId();
+        startFlag->test_and_set();
 
-		if (!::RegisterHotKey(nullptr, hotKeyId, modifiers, key))
-		{
-			DebugLog(std::format("Hotkey (id: {0}) > Failed to register hot key.\n", std::to_string(hotKeyId)));
-			//threadFlag.test_and_set();
-			startFlag->notify_one();
-		}
-		else
-		{
-			DebugLog(std::format("Hotkey (id: {0}) > Registered.\n", std::to_string(hotKeyId)));
-			startFlag->test_and_set();
-			startFlag->notify_one();
+        if (!::RegisterHotKey(nullptr, hotKeyId, modifiers, key))
+        {
+            DebugLog(std::format("Hotkey (id: {0}) > Failed to register hot key.", std::to_string(hotKeyId)));
+            startFlag->notify_one();
+        }
+        else
+        {
+            DebugLog(std::format("Hotkey (id: {0}) > Registered.", std::to_string(hotKeyId)));
+            *failed = false;
+            startFlag->notify_one();
 
-			MSG message{};
-			while (1)
-			{
-				//PeekMessage()
-				if (GetMessage(&message, (HWND)(-1), 0, 0) & 1)
-				{
-					if (message.message == WM_HOTKEY)
-					{
-						DebugLog(std::format("Hotkey (id: {0}) > Hotkey pressed.\n", std::to_string(hotKeyId)));
+            MSG message{};
+            while (1)
+            {
+                //PeekMessage()
+                if (GetMessage(&message, (HWND)(-1), 0, 0) & 1)
+                {
+                    if (message.message == WM_HOTKEY)
+                    {
+                        if (keyEnabled.load())
+                        {
+                            e_fired(winrt::Windows::Foundation::IInspectable(), winrt::guid());
+                        }
+                    }
+                }
+                else
+                {
+                    DebugLog(std::format("Hotkey (id: {0}) > GetMessage returned 0.", std::to_string(hotKeyId)));
+                    break;
+                }
+            }
 
-						if (keyEnabled.load())
-						{
-							e_fired(winrt::Windows::Foundation::IInspectable(), winrt::guid());
-						}
-					}
-				}
-				else
-				{
-					DebugLog(std::format("Hotkey (id: {0}) > GetMessage returned 0.\n", std::to_string(hotKeyId)));
-					break;
-				}
-			}
+            // Unregister the HotKey when exiting the thread.
+            UnregisterHotKey(nullptr, hotKeyId);
 
-			// Unregister the HotKey when exiting the thread.
-			UnregisterHotKey(nullptr, hotKeyId);
-
-			DebugLog(std::format("Hotkey (id: {0}) > thread exiting.\n", std::to_string(hotKeyId)));
-		}
-	}
+            DebugLog(std::format("Hotkey (id: {0}) > thread exiting.", std::to_string(hotKeyId)));
+        }
+    }
 }
--- Croak/Hotkey.h
+++ Croak/Hotkey.h
@@ -3,77 +3,95 @@
 
 namespace Croak::System::HotKeys
 {
-	class HotKey
-	{
-	public:
-		HotKey() = delete;
-		HotKey(const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& key, const bool& isRepeating);
-		~HotKey();
+    class HotKey
+    {
+    public:
+        HotKey() = delete;
+        HotKey(const winrt::Windows::System::VirtualKeyModifiers& modifiers, const uint32_t& key, const bool& isRepeating);
+        ~HotKey();
 
-		/**
-		 * @brief Key.
-		 * @return The VK_[key name] associated with this hotkey
-		*/
-		inline uint32_t Key()
-		{
-			return key;
-		}
+        inline uint32_t Modifiers() const
+        {
+            return modifiers;
+        }
 
-		/**
-		 * @brief If the key is not enabled, Fired event will not be fired when the hotkey is pressed.
-		 * @return true if the hotkey is enabled
-		*/
-		inline bool Enabled()
-		{
-			return keyEnabled.load();
-		}
+        /**
+         * @brief Key.
+         * @return The VK_[key name] associated with this hotkey
+        */
+        inline uint32_t Key() const
+        {
+            return key;
+        }
 
-		/**
-		 * @brief If the key is not enabled, Fired event will not be fired when the hotkey is pressed.
-		 * @param enabled set to true to disable events
-		*/
-		inline void Enabled(const bool& enabled)
-		{
-			keyEnabled.exchange(enabled);
-		}
+        /**
+         * @brief If the key is not enabled, Fired event will not be fired when the hotkey is pressed.
+         * @return true if the hotkey is enabled
+        */
+        inline bool Enabled() const
+        {
+            return keyEnabled.load();
+        }
 
-		inline uint32_t KeyId()
-		{
-			return hotKeyId;
-		}
+        /**
+         * @brief If the key is not enabled, Fired event will not be fired when the hotkey is pressed.
+         * @param enabled set to true to disable events
+        */
+        inline void Enabled(const bool& enabled)
+        {
+            keyEnabled.exchange(enabled);
+        }
+
+        /**
+         * @brief Unique identifier for that key.
+         * @return 
+        */
+        inline uint32_t KeyId() const
+        {
+            return hotKeyId;
+        }
 
 
-		inline winrt::event_token Fired(const winrt::Windows::Foundation::TypedEventHandler<winrt::Windows::Foundation::IInspectable, winrt::guid>& handler)
-		{
-			return e_fired.add(handler);
-		}
+        /**
+         * @brief Fired event subscriber.
+         * @param handler callback
+         * @return Event token
+        */
+        inline winrt::event_token Fired(const winrt::Windows::Foundation::TypedEventHandler<winrt::Windows::Foundation::IInspectable, winrt::guid>& handler)
+        {
+            return e_fired.add(handler);
+        }
 
-		inline void Fired(const winrt::event_token& token)
-		{
-			e_fired.remove(token);
-		}
+        /**
+         * @brief Fired event revoker.
+         * @param token 
+        */
+        inline void Fired(const winrt::event_token& token)
+        {
+            e_fired.remove(token);
+        }
 
-		/**
-		 * @brief Activates the key to fire events.
-		*/
-		void Activate();
+        /**
+         * @brief Activates the key to fire events.
+        */
+        void Activate();
 
-	private:
-		static std::atomic_int32_t globalIds;
+    private:
+        static std::atomic_int32_t globalIds;
 
-		uint32_t key;
-		bool repeating;
-		uint32_t modifiers{};
-		uint32_t hotKeyId = 0;
-		std::thread* notificationThread = nullptr;
-		std::atomic_bool threadRunning = false;
-		std::atomic_bool keyEnabled = true;
-		DWORD threadId = 0ul;
+        uint32_t key;
+        bool repeating;
+        uint32_t modifiers = 0;
+        uint32_t hotKeyId = 0;
+        std::thread* notificationThread = nullptr;
+        std::atomic_bool threadRunning = false;
+        std::atomic_bool keyEnabled = true;
+        DWORD threadId = 0ul;
 
-		winrt::event<winrt::Windows::Foundation::TypedEventHandler<winrt::Windows::Foundation::IInspectable, winrt::guid>> e_fired {};
+        winrt::event<winrt::Windows::Foundation::TypedEventHandler<winrt::Windows::Foundation::IInspectable, winrt::guid>> e_fired {};
 
-		bool CheckParametersValidity(const DWORD& modifiers, const DWORD& key);
-		void ThreadFunction(std::atomic_flag* startFlag);
-	};
+        bool CheckParametersValidity(const DWORD& modifiers, const DWORD& key);
+        void ThreadFunction(std::atomic_flag* startFlag, bool* failed);
+    };
 }
 

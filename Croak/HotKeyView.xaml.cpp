167ef8066e01f622bf05fe6e5d330ff84292c89f
psykomicron <psykomicron@protonmail.com>
2023-01-31 13:03:47 +0100

    Changed HotKeyView font, added button to edit/save key and according events

--- Croak/HotKeyView.xaml.cpp
+++ Croak/HotKeyView.xaml.cpp
@@ -19,6 +19,15 @@ namespace winrt::Croak::implementation
         InitializeComponent();
     }
 
+    HotKeyView::HotKeyView(const HotkeyViewModel& model) :
+        _key{ model.Key() },
+        _modifiers{ model.Modifiers() },
+        _isHotKeyEnabled{ model.IsHotKeyEnabled() },
+        _hotKeyName{ model.HotKeyName() }
+    {
+        InitializeComponent();
+    }
+
     IInspectable HotKeyView::KeyName() const
     {
         int32_t keyValue = static_cast<int32_t>(_key);
@@ -33,9 +42,76 @@ namespace winrt::Croak::implementation
         }
     }
 
+
+    winrt::event_token HotKeyView::PropertyChanged(winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& value)
+    {
+        return e_propertyChanged.add(value);
+    }
+
+    void HotKeyView::PropertyChanged(winrt::event_token const& token)
+    {
+        e_propertyChanged.remove(token);
+    }
+
+    winrt::event_token HotKeyView::VirtualModifiersChanged(const Windows::Foundation::TypedEventHandler<Croak::HotKeyView, Windows::System::VirtualKeyModifiers>& handler)
+    {
+        return e_virtualKeyModifiersChanged.add(handler);
+    }
+
+    void HotKeyView::VirtualModifiersChanged(const winrt::event_token& token)
+    {
+        e_virtualKeyModifiersChanged.remove(token);
+    }
+
+    winrt::event_token HotKeyView::VirtualKeyChanged(const Windows::Foundation::TypedEventHandler<Croak::HotKeyView, Windows::System::VirtualKey>& handler)
+    {
+        return e_virtualKeyChanged.add(handler);
+    }
+
+    void HotKeyView::VirtualKeyChanged(const winrt::event_token& token)
+    {
+        e_virtualKeyChanged.remove(token);
+    }
+
+    winrt::event_token HotKeyView::Toggled(const Windows::Foundation::TypedEventHandler<Croak::HotKeyView, bool>& handler)
+    {
+        return e_toggled.add(handler);
+    }
+
+    void HotKeyView::Toggled(const winrt::event_token& token)
+    {
+        e_toggled.remove(token);
+    }
+
+
     void HotKeyView::OnKeyDown(const winrt::Microsoft::UI::Xaml::Input::KeyRoutedEventArgs&)
     {
         OutputDebugHString(L"Key pressed.");
+    }
+
+    void HotKeyView::ControlToggleButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        VirtualKeyModifiers oldModifiers = _modifiers;
+        _modifiers = GetModifiers();
+
+        if (oldModifiers != _modifiers)
+        {
+            e_virtualKeyModifiersChanged(*this, oldModifiers);
+        }
+    }
+
+    void HotKeyView::UserControl_Loaded(IInspectable const&, RoutedEventArgs const&)
+    {
+        SetModifiers();
+        loaded.store(true);
+    }
+
+    void HotKeyView::ToggleSwitch_Toggled(IInspectable const&, RoutedEventArgs const&)
+    {
+        if (loaded.load())
+        {
+            e_toggled(*this, EnabledToggleSwitch().IsOn());
+        }
     }
 
 
@@ -45,5 +121,16 @@ namespace winrt::Croak::implementation
         AltToggleButton().IsChecked((_modifiers & VirtualKeyModifiers::Menu) == VirtualKeyModifiers::Menu);
         ShiftToggleButton().IsChecked((_modifiers & VirtualKeyModifiers::Shift) == VirtualKeyModifiers::Shift);
         WindowsToggleButton().IsChecked((_modifiers & VirtualKeyModifiers::Windows) == VirtualKeyModifiers::Windows);
+    }
+
+    VirtualKeyModifiers HotKeyView::GetModifiers()
+    {
+        VirtualKeyModifiers modifiers = VirtualKeyModifiers::None;
+        modifiers |= ControlToggleButton().IsChecked().GetBoolean() ? VirtualKeyModifiers::Control : VirtualKeyModifiers::None;
+        modifiers |= AltToggleButton().IsChecked().GetBoolean()     ? VirtualKeyModifiers::Menu    : VirtualKeyModifiers::None;
+        modifiers |= ShiftToggleButton().IsChecked().GetBoolean()   ? VirtualKeyModifiers::Shift   : VirtualKeyModifiers::None;
+        modifiers |= WindowsToggleButton().IsChecked().GetBoolean() ? VirtualKeyModifiers::Windows : VirtualKeyModifiers::None;
+
+        return modifiers;
     }
 }
--- Croak/HotKeyView.xaml.h
+++ Croak/HotKeyView.xaml.h
@@ -12,75 +12,85 @@ namespace winrt::Croak::implementation
 {
     struct HotKeyView : HotKeyViewT<HotKeyView>
     {
+    public:
         HotKeyView();
+        HotKeyView(const Croak::HotkeyViewModel& model);
+
 
         inline winrt::Windows::System::VirtualKeyModifiers Modifiers() const
         {
             return _modifiers;
-        };
+        }
+
         inline void Modifiers(const winrt::Windows::System::VirtualKeyModifiers& value)
         {
             _modifiers = value;
             SetModifiers();
-        };
+        }
 
         inline winrt::Windows::System::VirtualKey Key() const
         {
             return _key;
-        };
+        }
+
         inline void Key(const winrt::Windows::System::VirtualKey& value)
         {
             _key = value;
-#ifdef DEBUG
-            IconContentControl().Content(KeyName());
-#else
             e_propertyChanged(*this, winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs(L"KeyName"));
-#endif // DEBUG
-        };
+        }
 
         inline bool IsHotKeyEnabled() const
         {
             return _isHotKeyEnabled;
-        };
+        }
+
         inline void IsHotKeyEnabled(const bool& value)
         {
             _isHotKeyEnabled = value;
             e_propertyChanged(*this, winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs(L"IsHotKeyEnabled"));
-        };
+        }
 
         inline winrt::hstring HotKeyName() const
         {
             return _hotKeyName;
-        };
+        }
+
         inline void HotKeyName(const winrt::hstring& value)
         {
             _hotKeyName = value;
             e_propertyChanged(*this, winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventArgs(L"HotKeyName"));
-        };
+        }
 
         winrt::Windows::Foundation::IInspectable KeyName() const;
 
-
-        winrt::event_token PropertyChanged(winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& value)
-        {
-            return e_propertyChanged.add(value);
-        };
-        void PropertyChanged(winrt::event_token const& token)
-        {
-            e_propertyChanged.remove(token);
-        };
+        winrt::event_token PropertyChanged(winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& value);
+        void PropertyChanged(winrt::event_token const& token);
+        winrt::event_token VirtualModifiersChanged(const Windows::Foundation::TypedEventHandler<Croak::HotKeyView, Windows::System::VirtualKeyModifiers>& handler);
+        void VirtualModifiersChanged(const winrt::event_token& token);
+        winrt::event_token VirtualKeyChanged(const Windows::Foundation::TypedEventHandler<Croak::HotKeyView, Windows::System::VirtualKey>& handler);
+        void VirtualKeyChanged(const winrt::event_token& token);
+        winrt::event_token Toggled(const Windows::Foundation::TypedEventHandler<Croak::HotKeyView, bool>& handler);
+        void Toggled(const winrt::event_token& token);
 
         void OnKeyDown(const winrt::Microsoft::UI::Xaml::Input::KeyRoutedEventArgs& args);
+        void ControlToggleButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void UserControl_Loaded(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void ToggleSwitch_Toggled(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
 
     private:
+        std::atomic_bool loaded = false;
         winrt::Windows::System::VirtualKeyModifiers _modifiers = winrt::Windows::System::VirtualKeyModifiers::None;
         winrt::Windows::System::VirtualKey _key = winrt::Windows::System::VirtualKey::None;
         bool _isHotKeyEnabled = false;
         winrt::hstring _hotKeyName{};
 
-        winrt::event<winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> e_propertyChanged;
+        winrt::event<winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> e_propertyChanged{};
+        winrt::event<Windows::Foundation::TypedEventHandler<Croak::HotKeyView, Windows::System::VirtualKeyModifiers>> e_virtualKeyModifiersChanged{};
+        winrt::event<Windows::Foundation::TypedEventHandler<Croak::HotKeyView, Windows::System::VirtualKey>> e_virtualKeyChanged{};
+        winrt::event<Windows::Foundation::TypedEventHandler<Croak::HotKeyView, bool>> e_toggled{};
 
         void SetModifiers();
+        Windows::System::VirtualKeyModifiers GetModifiers();
     };
 }
 
